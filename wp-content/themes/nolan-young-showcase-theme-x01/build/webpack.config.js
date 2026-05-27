937353cc26928be9673e6671f6bc6766831bff55
Nolan Young <nolanyoung@nicelawns.local>
2026-05-27 17:11:15 -0400

    Add nolan-young-showcase-theme-x01

---
+++ wp-content/themes/nolan-young-showcase-theme-x01/build/webpack.config.js
@@ -0,0 +1,260 @@
+﻿
+
+
+
+
+
+
+
+```javascript
+
+
+
+
+
+
+
+
+const path = require('path');
+
+
+
+
+
+
+
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+
+
+
+
+
+
+
+
+  entry: './src/js/main.js',
+
+
+
+
+
+
+
+
+  output: {
+
+
+
+
+
+
+
+
+    filename: 'bundle.js',
+
+
+
+
+
+
+
+
+    path: path.resolve(__dirname, '../assets/js'),
+
+
+
+
+
+
+
+
+  },
+
+
+
+
+
+
+
+
+  module: {
+
+
+
+
+
+
+
+
+    rules: [
+
+
+
+
+
+
+
+
+      {
+
+
+
+
+
+
+
+
+        test: /\.scss$/,
+
+
+
+
+
+
+
+
+        use: [
+
+
+
+
+
+
+
+
+          MiniCssExtractPlugin.loader,
+
+
+
+
+
+
+
+
+          'css-loader',
+
+
+
+
+
+
+
+
+          'sass-loader',
+
+
+
+
+
+
+
+
+        ],
+
+
+
+
+
+
+
+
+      },
+
+
+
+
+
+
+
+
+    ],
+
+
+
+
+
+
+
+
+  },
+
+
+
+
+
+
+
+
+  plugins: [
+
+
+
+
+
+
+
+
+    new MiniCssExtractPlugin({
+
+
+
+
+
+
+
+
+      filename: '../assets/css/main.css',
+
+
+
+
+
+
+
+
+    }),
+
+
+
+
+
+
+
+
+  ],
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
